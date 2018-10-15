5f4d0c7ed59d4105c863db14a3a1201c212a3762
Max.Burishinets <arhor18@gmail.com>
2018-10-15 14:53:20 +0300

    InputValidator tests started

--- src/by/epam/admission/util/InputValidator.java
+++ src/by/epam/admission/util/InputValidator.java
@@ -15,15 +15,15 @@ public class InputValidator {
     private static final String COUNTRY_CITY_PATTERN = "^[-а-яА-ЯёЁa-zA-Z]{2,55}$";
     private static final String CERTIFICATE_PATTERN = "^([0-9]{1,2})|(100)$";
     private static final String FIRST_LAST_NAME_PATTERN = "^[-а-яА-ЯёЁa-zA-Z]{2,35}$";
-    private static final String EMAIL_PATTERN =
-            "[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@" +
-                    "([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\\.)*([a-z]{2,4})";
-    private static final String PASSWORD_PATTERN =
-            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{6,10}$";
+    private static final String EMAIL_PATTERN = "^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\\.)*([a-z]{2,4})$";
+    private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{6,10}$";
+
+    private static final int EMAIL_MAX_LENGTH = 129;
 
     private InputValidator() {}
 
-    public static boolean validate(String input, InputType type) throws ProjectException {
+    public static boolean validate(String input, InputType type)
+            throws ProjectException {
         boolean result = false;
         String currentPattern;
         if (input != null && type != null) {
@@ -40,6 +40,9 @@ public class InputValidator {
                     currentPattern = FIRST_LAST_NAME_PATTERN;
                     break;
                 case EMAIL:
+                    if (input.length() > EMAIL_MAX_LENGTH) {
+                        return false;
+                    }
                     currentPattern = EMAIL_PATTERN;
                     break;
                 case PASSWORD:
